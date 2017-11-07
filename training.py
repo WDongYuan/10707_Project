db46d42ba1e0e52b1c5d0a49685d11505914a29c
Yazhi Gao <leon.gao19@gmail.com>
2017-11-07 13:39:36 -0500

    fix import in training

--- training.py
+++ training.py
@@ -1,7 +1,9 @@
 from model import baseline
 from model import relational_network_model
-from util import data
-from util import utils
+import sys
+sys.path.append('./util')
+import data
+import utils
 from tqdm import *
 import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
@@ -11,16 +13,14 @@ import torch.nn.functional as F
 import pdb
 import torch
 import torch.nn.utils
-import sys
 
-sys.path.append('./util')
 base = "ibowimg"
 rn = "rn"
 
 if sys.argv[1]==base:
-    import util.config_baseline as config
+    import config_baseline as config
 elif sys.argv[1]==rn:
-    from util import config
+    import config
 
 if __name__=="__main__":
     train = True
