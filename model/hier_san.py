fix softmax dim

model/hier_san.py
@@ -58,8 +58,8 @@ class hier_san(nn.Module):
             out_q = self.linear_q(out_q.view(-1,self.lstm_hidden_size)).view(-1,self.feat_hidden_size,seq_size) # (b, h, l) * (b, h, l) and (b, h, l) dot (k, h ) -> (b,k,l)
             h_i = F.tanh(out_i + torch.bmm(out_q,c)) # (b, k, s)
             h_q = F.tanh(out_q + torch.bmm(out_i,c.transpose(1,2))) #(b, k, l)
-            a_q = F.softmax(self.att_q(h_q.transpose(1,2)).squeeze()) # (b, l)
-            a_i = F.softmax(self.att_i(h_i.transpose(1,2)).squeeze()) # (b, s)
+            a_q = F.softmax(self.att_q(h_q.transpose(1,2)).squeeze()).unsqueeze(2) # (b, l)
+            a_i = F.softmax(self.att_i(h_i.transpose(1,2)).squeeze()).unsqueeze(2) # (b, s)
 
         out_i = torch.bmm(q.transpose(1,2),a_q).squeeze() # (b, h, len) * (b, len, 1) -> (b, h, 1)
         out_q = torch.bmm(v,a_i).squeeze()