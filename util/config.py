cce57bc35a4fbddbebfd8eade685a9995253a994
Yazhi Gao <leon.gao19@gmail.com>
2017-11-12 15:47:44 -0500

    free memory

--- util/config.py
+++ util/config.py
@@ -3,7 +3,7 @@ qa_path = 'data'  # directory containing the question and annotation jsons
 train_path = '../data/mscoco/train2014'  # directory of training images
 val_path = '../data/mscoco/val2014'  # directory of validation images
 test_path = '../data/mscoco/test2015'  # directory of test images
-preprocessed_path = '/10707data/resnet-14x14.h5'  # path where preprocessed features are saved to and loaded from
+preprocessed_path = '/10707data/resnet-7x7.h5'  # path where preprocessed features are saved to and loaded from
 numpy_path = './data/features_id.npz'
 vocabulary_path = 'data/vocab/vocab.json'  # path where the used vocabularies for question and answers are saved to
 
@@ -14,7 +14,7 @@ dataset = 'mscoco'
 # preprocess config
 preprocess_batch_size = 64
 image_size = 448 #for google paper  # scale shorter end of image to this size and centre crop
-output_size = 14  # size of the feature maps after processing through a network
+output_size = 7  # size of the feature maps after processing through a network
 output_features = 2048 # number of feature maps thereof
 central_fraction = 0.875  # only take this much of the centre when scaling and centre cropping
 word_embed_dim = 300
