a696723cd550ad6280f9d55370186c975e264a79
Yazhi Gao <leon.gao19@gmail.com>
2017-11-12 17:11:10 -0500

    fix parallel

--- training.py
+++ training.py
@@ -44,7 +44,7 @@ if __name__=="__main__":
         os.environ["CUDA_VISIBLE_DEVICES"] = "0"
         model.cuda()
     lr = float(sys.argv[2])
-    embed_params = list(map(id, model.embed.parameters()))
+    embed_params = list(map(id, model.module.embed.parameters()))
     base_params = filter(lambda p: id(p) not in embed_params,model.parameters())
     optimizer = optim.RMSprop([
                             {'params':model.embed.parameters(),'lr': config.initial_embed_lr},
