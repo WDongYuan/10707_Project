b46ab99e42c8b94f399273187a654a914ed4471f
WeidongYuan <weidongy@andrew.cmu.edu>
2017-11-21 16:45:08 -0500

    debug

--- training.py
+++ training.py
@@ -81,8 +81,8 @@ if __name__=="__main__":
         model = torch.load("./my_best_model_new.model")
     #########################################################################
     lr = float(sys.argv[2])
-    # optimizer = optim.Adam(model.parameters(),lr = lr)
-    optimizer = torch.optim.SGD([p for p in model.parameters() if p.requires_grad], lr=lr, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(),lr = lr)
+    # optimizer = torch.optim.SGD([p for p in model.parameters() if p.requires_grad], lr=lr, momentum=0.9)
 
     best_perf = 0.0
     if int(sys.argv[1]) == 2:    
